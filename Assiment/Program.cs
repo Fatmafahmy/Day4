7036058f9afc477adf9b6f3a3ed75e50ce2e7dda
10 <10@DESKTOP-MH5TSNA>
2024-12-21 17:25:44 +0200

    Add project files.

---
+++ Assiment/Program.cs
@@ -0,0 +1,399 @@
+﻿using System.ComponentModel.Design;
+using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        #region Q1 Print Nom btween 1 to Nom
+        //int Number;
+        //bool Flag;
+        //Console.WriteLine("Enter the Number");
+        //Flag = int.TryParse(Console.ReadLine(), out Number);
+        //for (int i = 1; i <= Number; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+        #endregion
+
+        #region Q2 insert Nom then * table up to 12
+        //int Number;
+        //bool Flag;
+        //Console.WriteLine("Enter the Number");
+        //Flag = int.TryParse(Console.ReadLine(), out Number);
+        //for (int i = 1; i <= 12; i++)
+        //{
+        //    Console.WriteLine($"{Number*i}");
+        // }
+        #endregion
+
+        #region Q3 insert number then print all even numbers between 1 to this number
+        //int Number;
+        //bool Flag;
+        //Console.WriteLine("Enter the Number");
+        //Flag = int.TryParse(Console.ReadLine(), out Number);
+        //for (int i = 2; i<= Number ; i +=2)
+        //    {
+        //       Console.WriteLine(i);
+        //   }
+
+        #endregion
+
+        #region Q4 takes two integers then prints the power
+        //Console.WriteLine("Enter the Base Number");
+        //int BaseNumber = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter the Base Power  Number");
+        //int powerNumber = int.Parse(Console.ReadLine());
+        //int result = 1;
+        //for (int i = 0; i < powerNumber; i++)
+        //{
+        //    result *= BaseNumber;
+        //}
+        //Console.WriteLine($"Result: {result}");
+
+        #endregion
+
+        #region Q5 enter a string and print the REVERSE of it
+        //Console.WriteLine("Enter the Word");
+        //string Word = Console.ReadLine();
+        //char[] charArray = Word.ToCharArray();
+        //Array.Reverse(charArray);
+        //string reversed = new string(charArray);
+        //Console.WriteLine(reversed);
+
+        #endregion
+
+        #region Q6 find prime numbers within a range of numbers
+        //int Number, Value = 0;
+        //Console.WriteLine("Enter Number"); 
+        //Number = int.Parse(Console.ReadLine());
+        //for (int i = 1; i < Number; i++)
+        //{
+        //    for (int j = 1; j < Number; j++)
+        //    {
+        //          if (i % j == 0)
+        //         {
+        //            Value++;
+        //        }
+        //    }
+        //    if (Value == 2)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //    Value = 0;
+        //}
+
+        #endregion
+
+        #region Q7 convert a decimal number into binary without using an array
+        //Console.WriteLine("Enter the  Number");
+        //int Number = int.Parse(Console.ReadLine());
+        //if (Number == 0)
+        //{
+        //    Console.WriteLine("0");
+        //}
+        //string Binary="";
+        //while(Number> 0)
+        //{
+        //    Binary = (Number % 2) + Binary;
+        //    Number /= 2;
+        //}
+        //Console.WriteLine(Binary);
+
+        #endregion
+
+
+        #region Q8 prints an identity matrix using for loop
+        //Console.WriteLine("Enter size of Matrix");
+        //int size =int.Parse(Console.ReadLine());
+        //for (int i = 0; i < size; i++)
+        //{
+        //    for (int j = 0; j < size; j++)
+        //    {
+        //        if (i == j)
+        //            Console.WriteLine("1");
+        //        else Console.WriteLine("0");
+        //    }
+        //    Console.WriteLine();
+        //}
+        #endregion
+
+        #region Q9 Write C# program that Extract a substring from a given string.
+
+        //Console.WriteLine("Enter the Syntic");
+        //string Word = Console.ReadLine();
+        //Console.WriteLine("Enter the index you want");
+        //int Index_of_Word = int.Parse(Console.ReadLine());
+        //if (Index_of_Word >= 0 && Index_of_Word < Word.Length)
+
+        //    {
+        //    string New_Word = Word.Substring(Index_of_Word);
+        //    Console.WriteLine(New_Word);
+        //}
+        //else
+        //{
+        //    Console.WriteLine(" Invalid Start Postion");
+        //}
+        #endregion
+
+        #region Q 10 Write C# program that take two string variables and print them as one variable 
+        //Console.WriteLine("Enter the first word");
+        //string First_Word =Console.ReadLine();
+        //Console.WriteLine("Enter the Second word");
+        //string Second_Word = Console.ReadLine();
+        //string Full_Word =First_Word + Second_Word;
+        //Console.WriteLine ( "The Full Word is " +Full_Word);
+
+        #endregion
+
+        #region Q11 It is The Same As Question 8
+        //Console.WriteLine("Enter size of Matrix");
+        //int size =int.Parse(Console.ReadLine());
+        //for (int i = 0; i < size; i++)
+        //{
+        //    for (int j = 0; j < size; j++)
+        //    {
+        //        if (i == j)
+        //            Console.WriteLine("1");
+        //        else Console.WriteLine("0");
+        //    }
+        //    Console.WriteLine();
+        //}
+        #endregion
+
+        #region Q12 Write a program in C# Sharp to find the sum of all elements of the array.
+        //Console.WriteLine("Enter the Number Of Array");
+        //int Number = int.Parse(Console.ReadLine());
+        //int[]array = new int[Number];
+        //int Sum = 0;
+        //Console.WriteLine("Enter Element Of Array");
+        //for (int i = 0; i < Number; i++)
+        //{
+        //    Console.WriteLine($"Element{i+1}");
+        //    array[i]=int.Parse(Console.ReadLine());
+        //    Sum +=array[i];
+        //    Console.WriteLine(Sum);
+        //}    
+        #endregion
+
+        #region Q13  Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+        //Console.WriteLine("Enter the Size Of Array");
+        //int Size = int.Parse(Console.ReadLine());
+        //int[] array1 = new int[Size];
+        //int[] array2 = new int[Size];
+        //int[] Merge_Array = new int[Size *2];
+        //Console.WriteLine("Enter the Element about Array1");
+        //for (int i = 0; i < Size; i++)
+        //{
+        //    Console.Write($"Element {i + 1} ");
+        //    array1[i] = int.Parse(Console.ReadLine());
+        //}
+        //Console.WriteLine("Enter the Element about Array2");
+        //for (int i = 0; i < Size; i++)
+        //{
+        //    Console.Write($"Element {i + 1} ");
+        //    array2[i] = int.Parse(Console.ReadLine());
+        //}
+
+        //   for(int i=0; i<Size; i++)
+        //    {
+        //        Merge_Array[i] = array1[i];
+        //        Merge_Array[i+Size] =array2[i];
+        //    }
+        //   Array.Sort(Merge_Array);
+        //    Console.WriteLine("Merage is");
+        //foreach (int element in Merge_Array) {
+        //    Console.WriteLine(element);
+
+        //}
+
+        #endregion
+
+        #region Q14 Write a program in C# Sharp to count the frequency of each element of an array.
+        //Console.WriteLine("Entr Size of Array");
+        //int Size = int.Parse(Console.ReadLine());
+        //int[] arr1 = new int[Size];
+        //bool[] visited = new bool[Size];
+
+        //Console.WriteLine("Enter the Element of Array");
+        //for (int i = 0; i < Size; i++)
+        //{
+        //    Console.WriteLine($"Element {i + 1}");
+        //    arr1[i] = int.Parse(Console.ReadLine());
+        //}
+        //Console.WriteLine("The Number of Array ");
+        //Console.WriteLine(string.Join(", ", arr1));
+
+        //Console.WriteLine("Number anf Frequency");
+        //for (int i = 0; i < Size; i++)
+        //{
+        //    if (visited[i]) continue;
+        //    int count = 1;
+        //    for (int j = i + 1; j < Size; j++)
+        //    {
+        //        if (arr1[i] == arr1[j])
+        //        {
+        //            count++;
+        //            visited[j] = true; 
+        //        }
+        //    }
+        //    Console.WriteLine($"Number {arr1[i]} appears {count} ");
+        //}
+
+
+        #endregion
+
+
+        #region Q15 Write a program in C# Sharp to find maximum and minimum element in an array
+        //Console.WriteLine("Entr Size of Array");
+        //int Size = int.Parse(Console.ReadLine());
+        //int[] arr = new int[Size];
+
+        //Console.WriteLine("Enter the Element of Array");
+        //for (int i = 0; i < Size; i++)
+        //{
+        //    Console.WriteLine($"Element {i + 1}");
+        //    arr[i] = int.Parse(Console.ReadLine());
+        //}
+        //int Max = arr[0];
+        //int Min = arr[0];
+        //for (int i = 1; i < Size; i++)
+        //{
+        //   if (arr[i] > Max)
+        //    {
+        //        Max = arr[i];
+        //    }
+        //    if (arr[i] < Min)
+        //    {
+        //        Min = arr[i];
+        //    }
+        //}
+        //Console.WriteLine($"Max Element in the Array Array{Max}");
+        //Console.WriteLine($"Min Element in The Arrar Array{Min}");
+        #endregion
+
+
+        #region Q16 - Write a program in C# Sharp to find the second largest element in an array.
+        //Console.WriteLine("Entr Size of Array");
+        //int Size = int.Parse(Console.ReadLine());
+        //int[] arr = new int[Size];
+
+        //Console.WriteLine("Enter the Element of Array");
+        //for (int i = 0; i < Size; i++)
+        //{
+        //    Console.WriteLine($"Element {i + 1}");
+        //    arr[i] = int.Parse(Console.ReadLine());
+        //}
+        //int Max = arr[0];
+        //int Maxindex = 0;
+
+        //for (int i = 1; i < Size; i++)
+        //{
+        //    if (arr[i] > Max)
+        //    {
+        //        Max = arr[i];
+        //        Maxindex = i;
+        //    }
+        //}
+        //arr[Maxindex] = -1;
+        //int Second_Largest =arr[0];
+        //for (int i = 1;i < Size; i++)
+        //{
+        //    if (arr[i] > Second_Largest)
+        //    { 
+        //        Second_Largest = arr[i];
+        //    }
+        //}
+        //Console.WriteLine($"The Second Largest Elemnt{Second_Largest}");
+        #endregion
+
+        #region Q17 Consider an Array of Integer values with size N, having values
+        //Console.WriteLine("Entr Size of Array");
+        //int Size = int.Parse(Console.ReadLine());
+        //int[] arr = new int[Size];
+
+        //Console.WriteLine("Enter the Element of Array");
+        //for (int i = 0; i < Size; i++)
+        //{
+        //    Console.WriteLine($"Element {i + 1}");
+        //    arr[i] = int.Parse(Console.ReadLine());
+        //}
+        //Dictionary<int,int>FirstIndex = new Dictionary<int,int>();
+        //int MaxDistanse = 0;
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    if (!FirstIndex.ContainsKey(arr[i]))
+        //    {
+        //        FirstIndex[arr[i]] = i;
+
+        //    }
+        //    else
+        //    {
+        //        int Distanse = i - FirstIndex[arr[i]];
+        //        MaxDistanse = Distanse;
+        //    }
+        //}
+        //Console.WriteLine($"The Largest Detanse{MaxDistanse}");
+
+        #endregion
+
+        #region Q18 create two multidimensional arrays of same size
+        //Console.WriteLine("Entr The NUmber of Rows");
+        //int Rows = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Entr The NUmber of Coulmns");
+        //int Coulmns = int.Parse(Console.ReadLine());
+
+        //int[,] arr1 = new int[Rows,Coulmns];
+        //int[,] arr2 = new int[Rows, Coulmns];
+
+        //Console.WriteLine("Enter the Element of Array");
+        //for (int i = 0; i < Rows; i++)
+        //{
+        //    for (int j = 0; j < Coulmns; j++)
+        //    {
+        //        Console.WriteLine($"Element[ {i},{j}]");
+        //        arr1[i, j] = int.Parse(Console.ReadLine());
+        //    }
+        //}
+        //for (int i = 0; i < Rows; i++)
+        //{
+        //    for (int j = 0; j < Coulmns; j++)
+        //    {
+        //        Console.WriteLine($"Element[ {i},{j}]");
+        //        arr2[i, j] = int.Parse(Console.ReadLine());
+        //    }
+        //}
+        //Console.WriteLine("Second Array");
+        //for(int i = 0;i < Rows; i++)
+        //{
+        //    for(int j = 0;j < Coulmns; j++)
+        //    {
+        //        Console.WriteLine(arr2[i, j]);
+        //    }
+        //}
+        #endregion
+
+        #region Q19 Write a Program to Print One Dimensional Array in Reverse Order
+        //Console.WriteLine("Entr Size of Array");
+        //int Size = int.Parse(Console.ReadLine());
+        //int[] arr = new int[Size];
+
+        //Console.WriteLine("Enter the Element of Array");
+        //for (int i = 0; i < Size; i++)
+        //{
+        //    Console.WriteLine($"Element {i + 1}");
+        //    arr[i] = int.Parse(Console.ReadLine());
+        //}
+        //Console.WriteLine(" Array in Reverse Order");
+        //for (int i = Size-1;i >= 0;i--)
+        //{
+        //    Console.WriteLine(arr[i]);
+        //}
+
+            #endregion
+
+
+        }
+    }
---
+++ Day4/Program.cs
@@ -0,0 +1,338 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Loop Statment 
+        //for (int i = 1; i <= 10; i++)
+        //{ 
+        //    Console.WriteLine($"{i} T");
+        //}
+        #endregion
+
+        #region For - Foreach
+        // int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // For loop it works on the original loop
+        //for (int i = 0; i < Numbers.Length; i++)
+        //{
+        //    // Numbers[i] += 10;
+        //    if (Numbers[i] == 5)
+        //    {
+        //        //break; // To Stop Array
+        //        continue; // print all Number but not 5
+        //    }
+
+        //    Console.WriteLine(Numbers[i] + 10);
+        //}
+
+        // Class Implinemt interface IEnummrable
+        // For each loop it works on a copy loop
+        //foreach (int Number in Numbers)
+        //{
+        //   // Numbers += 10; // Invalid
+        //    Console.WriteLine(Number +10);
+        //}
+        #endregion
+
+        #region While -Do While
+        //int Number;
+        //bool Flag;
+        //do  
+        //{
+        //    Console.WriteLine("Enter Even Number");
+        //     //Number = int.Parse(Console.ReadLine());
+        //    Flag= int.TryParse(Console.ReadLine(), out Number); 
+        //}
+        //while (Number %2 == 1 || !Flag);
+
+        //Console.WriteLine($"{Number} is Even");
+
+        //int Number = 3;
+        //bool Flag = false;
+        //while (Number % 2 == 1 || !Flag) 
+        //{
+        //    Console.WriteLine("Enter Even Number");
+        //     Flag= int.TryParse(Console.ReadLine(), out Number); 
+        // }
+        //Console.WriteLine($"{Number} is Even");
+
+        #endregion
+
+        #region  String
+        //Class => Referance type
+        //Immutable Data Tyoe[Value can not changed]
+        //Array of Chars
+        //A H M E D
+
+        //string Name;
+
+        //Name = new string("Ahmed");
+
+        //Name = "Ahmed"; //Syntax sugar 
+
+        //string Name01 = "Ahmed";
+        //string Name02 = "Ahmed";
+
+        //Console.WriteLine(Name01);
+        //Console.WriteLine($"Name01 HC: {Name01.GetHashCode()}");
+        //Console.WriteLine(Name02);
+        //Console.WriteLine($"Name02 HC: {Name02.GetHashCode()}");
+
+
+        //string Name01 = "Ahmed";
+        //string Name02 = "Ali";
+
+        //Console.WriteLine(Name01);
+        //Console.WriteLine($"Name01 HC: {Name01.GetHashCode()}");
+        //Console.WriteLine(Name02);
+        //Console.WriteLine($"Name02 HC: {Name02.GetHashCode()}");
+
+        //Name02 = Name01;
+
+        //Console.WriteLine("********** Name02 = Name01********");
+        //Console.WriteLine(Name01);
+        //Console.WriteLine($"Name01 HC: {Name01.GetHashCode()}");
+        //Console.WriteLine(Name02);
+        //Console.WriteLine($"Name02 HC: {Name02.GetHashCode()}");
+
+        //Name01= "Mostafa";
+
+        //Console.WriteLine("**********  Name01= Mostafa********");
+        //Console.WriteLine(Name01);
+        //Console.WriteLine($"Name01 HC: {Name01.GetHashCode()}");
+        //Console.WriteLine(Name02);
+        //Console.WriteLine($"Name02 HC: {Name02.GetHashCode()}");
+
+        //string Message = "Hello";
+        //Console.WriteLine(Message);
+        //Console.WriteLine(Message.GetHashCode());
+
+        //Message += "Route ";
+        //Console.WriteLine(Message);
+        //Console.WriteLine(Message.GetHashCode());
+
+        #endregion
+
+        #region StringBuilder
+        //StringBuilder Message;
+        //Message = new StringBuilder("Hello");
+        //Console.WriteLine(Message);
+        //Console.WriteLine(Message.GetHashCode());
+        //Message.Append("Route");
+        //Console.WriteLine(Message);
+        //Console.WriteLine(Message.GetHashCode());
+
+        #endregion
+
+        #region StringBuilder Methods
+        //StringBuilder Message = new StringBuilder("Welcome");
+
+        //Message.Append("To Route");
+        //Message.AppendLine("Mostafa");
+        //Message.Append("Hany");
+        //Console.WriteLine(Message);
+
+        //Message.Remove(0, 7);
+        //Console.WriteLine(Message);
+
+        //Message.Insert(0, "Hallo");
+        //Console.WriteLine(Message);
+
+        //Console.WriteLine();
+        //int age = 10;
+        //string Name = "Ali";
+        //Message.AppendFormat("Name : {0} , Age : {1}" ,Name,age);
+
+        //Message.AppendJoin("/", "Mostafa ", "Hany", "Mohamed", 2);
+        //Console.WriteLine(Message);
+        #endregion
+
+        #region Array 1D
+        //int[] Numbers = new int[3] {1, 2, 3};
+        //int[] Numbers = new int[] { 1, 2, 3 };
+        //int[] Numbers = { 1, 2, 3 };
+
+        //Console.WriteLine(Numbers[0]);
+        //Console.WriteLine(Numbers[1]);
+        //int[] Numbers = new int[3];
+
+        ////Numbers[0] = 1; 
+        ////Numbers[1] = 2;
+        ////Numbers[2] = 3;
+
+        //for (int i = 0; i < Numbers.Length; i++) 
+        //{
+        //    //Numbers[i] = i + 1;
+        //    Console.WriteLine($"Enter Element Number {i+1}");
+        //    Numbers[i] =int .Parse( Console.ReadLine() );
+        //}
+        //Console.WriteLine("**********");
+        //for (int i = 0; i < Numbers.Length; i++)
+        //{
+        //    Console.WriteLine(Numbers[i]);
+        //}
+        //Console.WriteLine(Numbers.Length);
+        //Console.WriteLine(Numbers.Rank); //Print Dimantion about Array
+
+        #endregion
+
+        #region Array 2D
+        // int[,] Marks = new int[3, 5];// { { 1,2,3,4,5},
+        //                             //{ 10,20,30,40,50},
+        //                             //{100,200,300,400,500 } }
+        //                             //Console.WriteLine(Marks.Length);
+        //                             //Console.WriteLine(Marks.Rank);
+        //                             //Console.WriteLine(Marks.GetLength(0));
+        //                             //Console.WriteLine(Marks.GetLength(1));
+
+        //for (int i = 0; i < Marks.GetLength(0); i++)
+        //{
+        //    Console.WriteLine($"Enter Grade Student({i + 1}");
+        //    for (int j = 0; j < Marks.GetLength(1);/* j++ */)
+        //    {
+        //        bool Flag;
+        //        Console.WriteLine($"Enter Grade Subject({j + 1}");
+        //        // Marks[i, j] = int.Parse(Console.ReadLine());
+        //        Flag = int.TryParse(Console.ReadLine(), out Marks[i, j]);
+        //        //if (Flag && Marks[i, j] >= 0)
+        //        //{
+        //        //    j++;
+        //        //}
+
+        //        j = Flag && Marks[i, j] >= 0 ? ++j : j; // We Cant use j++ because post fix we want to use pre fix
+        //    }
+        //}
+
+        //for (int i = 0; i < Marks.GetLength(0); i++)
+        //{
+        //    Console.WriteLine($"Grade Student({i + 1}");
+        //    for (int j = 0; j < Marks.GetLength(1); j++)
+        //    {
+        //        Console.WriteLine($"Grade Subject({j + 1}");
+        //        Console.WriteLine(Marks[i, j]);
+        //    }
+        //}
+
+        //Console.Clear();
+        //for (int i = 0; i < Marks.Length; i++)
+        //{
+        //    Console.WriteLine(Marks[i / Marks.GetLength(1), i % Marks.GetLength(1)]);
+        //}
+
+
+        #endregion
+
+        #region Judged Array
+        //int[][] Number = new int[3][];
+        //Number[0] = new int[3] {1,2,3};
+        // Number[1] = new int[2] {4,5};
+        //Number[2] = new int[1] {6};
+
+        //for (int i = 0; i < 3; i++)
+        //{
+        //    for (int j = 0; j < 2; j++)
+        //    {
+        //        Console.WriteLine(Number[i][j]);
+        //    }
+        //}
+
+        #endregion
+
+        #region Search About Array Method
+        #region C# Array Operations and Methods
+        //Baic OPeration
+        //int[] numbers = { 10, 20, 30, 40, 50 };
+
+        //Console.WriteLine(numbers[0]); // 10
+
+        //// Updating elements
+        //numbers[2] = 35;
+        //Console.WriteLine(numbers[2]);
+        #endregion
+
+        #region Array Initialization find lengh
+        //int[] numbers = new int[5]; // Creates an array of size 5
+        //numbers[0] = 1;
+
+        //// Initialize with values
+        //int[] initializedArray = { 1, 2, 3, 4, 5 };
+
+        //int length = numbers.Length; // Get the number of elements
+        //Console.WriteLine($"Length: {length}");
+        #endregion
+
+        #region Search for an Element
+        //int[] numbers = { 10, 20, 30, 40, 50 };
+
+
+        //int index = Array.IndexOf(numbers, 30); // Returns 2
+        //Console.WriteLine($"Index of 30: {index}");
+
+        //// Check if an element exists
+        //bool contains = numbers.Contains(40); // Returns true
+        //Console.WriteLine($"Contains 40: {contains}");
+        #endregion
+
+        #region Advanced Operations [Sorting, Reverse,Copy Elements]
+        //int[] numbers = { 50, 10, 40, 20, 30 };
+
+        //Array.Sort(numbers);
+        //Console.WriteLine(string.Join(", ", numbers)); // Output: 10, 20, 30, 40, 50
+
+        //Array.Reverse(numbers);
+        //Console.WriteLine(string.Join(", ", numbers)); // Output: 50, 40, 30, 20, 10
+
+        //int[] source = { 1, 2, 3, 4, 5 };
+        //int[] destination = new int[source.Length];
+
+        //// Copy elements
+        //Array.Copy(source, destination, source.Length);
+        //Console.WriteLine(string.Join(", ", destination)); // Output: 1, 2, 3, 4, 5
+
+        #endregion
+
+
+        #region LINQ Operations on Arrays[ Filter Elements,Find Max, Min, and Sum,. Transform Elements]
+
+        //int[] numbers = { 10, 20, 30, 40, 50 };
+        //var filtered = numbers.Where(n => n > 20).ToArray();
+        //Console.WriteLine(string.Join(", ", filtered)); // Output: 30, 40, 50
+        ////
+        //int max = numbers.Max(); // Maximum value
+        //int min = numbers.Min(); // Minimum value
+        //int sum = numbers.Sum(); // Sum of all elements
+
+        //Console.WriteLine($"Max: {max}, Min: {min}, Sum: {sum}");
+        ////
+        //var squared = numbers.Select(n => n * n).ToArray();
+        //Console.WriteLine(string.Join(", ", squared)); // Output: 100, 400, 900, 1600, 2500
+
+
+        #endregion
+
+
+        #region Cloning Arrays
+        int[] original = { 1, 2, 3 };
+        int[] cloned = (int[])original.Clone();
+
+        cloned[0] = 10; // Modify cloned array
+        Console.WriteLine(string.Join(", ", original)); // Output: 1, 2, 3
+        Console.WriteLine(string.Join(", ", cloned));   // Output: 10, 2, 3
+
+        #endregion
+
+        #region  Jagged Arrays
+        //int[][] jaggedArray = new int[3][];
+        //jaggedArray[0] = new int[] { 1, 2 };
+        //jaggedArray[1] = new int[] { 3, 4, 5 };
+        //jaggedArray[2] = new int[] { 6 };
+
+        //// Access elements
+        //Console.WriteLine(jaggedArray[1][2]); // 5
+
+        #endregion
+        #endregion
+    }
+}
